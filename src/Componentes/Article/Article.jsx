025ef3464f84f2ffe20b707bc406c136aad08f96
Wander-Cardoso <wander.hsmc@gmail.com>
2024-10-22 11:46:36 +0100

    tratativa da responsividade

--- src/Componentes/Article/Article.jsx
+++ src/Componentes/Article/Article.jsx
@@ -1,9 +1,9 @@
 const Article = ({ title, text, tags, image, alt }) => {
     
-    return <div className="card py-10 m-5 h-50 grid grid-cols-2 gap-4  ">
+    return <div className="card py-10 m-5 h-50 grid grid-cols-1 gap-4  ">
         
         <h3 className="text-xl text-nowrap text-alura-200 dark:text-gray-200 font-bold px-20  ">{title}</h3>
-        <div className=" hidden text-center sm:flex columns-1flex-shrink gap-2 ml-52">
+        <div className=" text-center flex columns-1flex-shrink gap-2 ml-52">
         {
             tags.map(tag => <span key={tag} className="bg-cyan-800  py-1.5 rounded-xl bg-gradient-to-r from-cyan-500 to-violet-500 font-bold text-xs  size-20 h-8 " >{tag}</span>)
         }
--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,12 +1,16 @@
 import ToggleTheme from "../Header/ToggleTheme/ToggleTheme"
 
+
+
 const Header = ({user}) => { 
 
-    return <div className="flex py-1.5 h-10 bg-alura-200 dark:bg-dark-200 text-pretty justify-between  px-20 gap-2 sm:rounded-xl sm:m-2 m-2 bg-scroll ">
-        <span className="text-cyan-300 md:text-lg max-[555px]:hidden ">Olá, { user?.name || "Usuário!"}</span>
-        <h1 className="  text-gray-100 text-lg font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-500 inline-block ">Bem vido (a), ao meu Portifólio!</h1>
+
+    return <div className="	flex items-center py-1.5 h-10 bg-alura-200 dark:bg-dark-200 text-pretty md:justify-between  gap-2 sm:rounded-xl ">
+        <span className="hidden md:block text-cyan-300   ">Olá, { user?.name || "Usuário!"}</span>
+        <h1 className="	justify-content: center text-gray-100 text-lg font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-500  ">Bem vido (a), ao meu Portifólio!</h1>
         <ToggleTheme />
         </div>
 }
+
 
 export default Header 
