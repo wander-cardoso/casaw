610b3cb436cd878dcd765ef9d8716a9d4e405693
Wander-Cardoso <wander.hsmc@gmail.com>
2024-10-23 10:46:26 +0100

    ajustes no header e na centralizacao dos componentes do body

--- src/Componentes/Article/Article.jsx
+++ src/Componentes/Article/Article.jsx
@@ -2,8 +2,8 @@ const Article = ({ title, text, tags, image, alt }) => {
     
     return <div className="card py-10 m-5 h-50 grid grid-cols-1 gap-4  ">
         
-        <h3 className="text-xl text-nowrap text-alura-200 dark:text-gray-200 font-bold px-20  ">{title}</h3>
-        <div className=" text-center flex justify-center columns-1flex-shrink gap-2 md:justify-start ml-12 ">
+        <h3 className="text-xl text-center md:text-start  text-nowrap text-alura-200 dark:text-gray-200 font-bold px-20  ">{title}</h3>
+        <div className="flex text-center  justify-center columns-1flex-shrink gap-2 md:justify-start md:ml-12 ">
         {
             tags.map(tag => <span key={tag} className="bg-cyan-800  py-1.5 rounded-xl bg-gradient-to-r from-cyan-500 to-violet-500 font-bold text-xs size-20 h-8 " >{tag}</span>)
         }
--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -5,7 +5,7 @@ import ToggleTheme from "../Header/ToggleTheme/ToggleTheme"
 const Header = ({user}) => { 
 
 
-    return <div className="	flex items-center py-1.5 h-10 bg-alura-200 dark:bg-dark-200 text-pretty md:justify-between  gap-2 sm:rounded-xl ">
+    return <div className="	flex justify-center py-1.5 h-10 bg-alura-200 dark:bg-dark-200 text-pretty md:justify-between  gap-2 sm:rounded-xl ">
         <span className="hidden md:block text-cyan-300   ">Olá, { user?.name || "Usuário!"}</span>
         <h1 className="	justify-content: center text-gray-100 text-lg font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-500  ">Bem vido (a), ao meu Portifólio!</h1>
         <ToggleTheme />
