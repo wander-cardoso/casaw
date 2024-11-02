7f6b3056910f864d7fbca3f96aacbc35ce76ca47
Wander-Cardoso <wander.hsmc@gmail.com>
2024-11-02 19:12:26 +0000

    alinhamento dos elementos do body para telas pequenas

--- src/Componentes/Home/Home.jsx
+++ src/Componentes/Home/Home.jsx
@@ -5,13 +5,13 @@ import MinhaImagem from "../img/eu.jpeg"
 
 const Home = () => {
 
-    return <div className="flex justify-center bg-sky-950 m-2 gap-20">
+    return <div className="flex md:justify-center bg-sky-950 m-2 md:gap-20">
 
-    <h1 className="dark:text-gray-50 scale-75 text-10 text-6xl p-20 py-10 ">Olá sou Wander, <br/> <strong>Desenvolverdor  Fullstack</strong></h1>
+    <h1 className="dark:text-gray-50 scale-75 text-10 md:text-6xl p-20 py-10 ">Olá sou Wander, <br/> <strong>Desenvolverdor  Fullstack</strong></h1>
 
     <span   className="flex py-10 m-6 ">  <DiReact /> <DiHtml5/> <DiJavascript/> <DiGit/> <DiCss3/> </span>
     
-    <img className="w-36  rounded-full" src={MinhaImagem} alt="" />
+    <img className=" w-20 md:w-36  rounded-full" src={MinhaImagem} alt="" />
 
 </div>
 
