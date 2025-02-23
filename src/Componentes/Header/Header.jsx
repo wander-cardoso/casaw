988f0b9c9fb7eddadde95efa400af91924ae6cfc
Wander-Cardoso <wander.hsmc@gmail.com>
2025-02-23 20:20:29 +0000

    atualizacao do back ground para "h-full"

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,11 +1,18 @@
 import ToggleTheme from "../Header/ToggleTheme/ToggleTheme"
-
+import { FaHouse } from "react-icons/fa6";
 
 
 const Header = () => { 
 
 
-    return <div className="	flex p-4 justify-center py-1.5 h-10 bg-alura-200 dark:bg-dark-200  sm::justify-between gap-2 sm:gap-28 ">
+    return <div className="	flex p-4 justify-around py-1.5 h-10 bg-alura-200 dark:bg-dark-200   gap-2 sm:gap-28 ">
+        <ul>
+            <li >
+                <FaHouse className="h-6  text-gray-400 block dark:hidden cursor-pointer"/>
+                <FaHouse className="h-6 text-yellow-200 hidden dark:block cursor-pointer"/>
+                <a href="http://localhost:5173/"></a>
+            </li>
+        </ul>
         <h1 className=" flex text-gray-100 text-xs sm:text-xl py-1 sm:py-0 font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-500  ">Bem vido (a), ao meu Portifólio!</h1>
         <ToggleTheme />
         </div>
--- src/Componentes/Home/Home.jsx
+++ src/Componentes/Home/Home.jsx
@@ -5,11 +5,11 @@ import MinhaImagem from "../img/eu.jpeg"
 
 const Home = () => {
 
-    return <div className="flex rounded-md sm:justify-center bg-sky-950 m-2 sm:gap-20">
+    return <div className="flex rounded-md justify-around bg-sky-950 m-2 sm:gap-20">
 
     <h1 className="dark:text-gray-50 scale-75 text-10 sm:text-6xl sm:p-20 sm:py-10 ">Olá sou Wander, <br/> <strong>Desenvolverdor  Fullstack</strong></h1>
 
-    <span   className="flex py-10 sm:m-6 ">  <DiReact /> <DiHtml5/> <DiJavascript/> <DiGit/> <DiCss3/> </span>
+    <span   className="flex justify-around py-10 sm:m-6 ">  <DiReact /> <DiHtml5/> <DiJavascript/> <DiGit/> <DiCss3/> </span>
     
     <img className="  h-30 w-10 sm:h-52 sm:w-36  rounded-full" src={MinhaImagem} alt="minhaimagem" />
 
